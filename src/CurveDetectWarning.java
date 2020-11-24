796ea0ed96bdf3543bb31ae3352d2ce5464b685e
Ashwin S <ashwins@Ashwins-MacBook-Pro.local>
2020-11-23 17:33:04 -0700

    Commit 1

---
+++ src/CurveDetectWarning.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CurveDetectWarning {
+    public ArrayList<ArrayList<String>> curveDetect(List<CurveObject> curveObjects){
+        ArrayList<ArrayList<String>> message = new ArrayList<>();
+        for(int i = 0;i<curveObjects.size();i++){
+            ArrayList<String> curveMessage = new ArrayList<>();
+            double time;
+            double distance;
+            time = Float.parseFloat(String.valueOf(curveObjects.get(i).getCurveExit()-
+                    curveObjects.get(i).getCurveEntry())) - 0.05;
+            curveMessage.add(""+time);
+            distance = computeDistance(time,curveObjects.get(i).getAverageSpeed());
+            curveMessage.add(""+distance);
+            message.add(curveMessage);
+        }
+        return message;
+    }
+    public double computeDistance(Double time, float speed){
+        double timeSeconds = time / 3600;
+        double computedDistance = Double.parseDouble(String.valueOf(speed)) * 0.05;
+        return computedDistance;
+    }
+}
---
+++ src/CurveObject.java
@@ -0,0 +1,47 @@
+public class CurveObject {
+    private float curveEntry;
+    private float curveExit;
+    private String type;
+    private float averageSpeed;
+    private String direction;
+
+    public float getCurveEntry() {
+        return curveEntry;
+    }
+
+    public void setCurveEntry(float curveEntry) {
+        this.curveEntry = curveEntry;
+    }
+
+    public float getCurveExit() {
+        return curveExit;
+    }
+
+    public void setCurveExit(float curveExit) {
+        this.curveExit = curveExit;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public float getAverageSpeed() {
+        return averageSpeed;
+    }
+
+    public void setAverageSpeed(float averageSpeed) {
+        this.averageSpeed = averageSpeed;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+}
